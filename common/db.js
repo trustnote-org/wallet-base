f05c0fe91f191a92e80daca1cd56e1936f0a4489
zhongjiewang <wangzhongjie@xindedata.com>
2018-06-20 11:05:58 +0800

    test

---
+++ common/db.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const tempdb = require('../common/db');
+
+class DataBase {
+
+    constructor() {
+        this.db = tempdb;
+    }
+
+    query (sqlstr, arr ) {
+        let resolve;
+        const waitPromise = new Promise(r => resolve = r);
+        if (arr) {
+            this.db.query(sqlstr, arr, function(rows){ 
+                resolve(rows);
+            })
+        } else {
+            this.db.query(sqlstr, function(rows){
+                resolve(rows);
+            })
+        }
+        return waitPromise;
+    }
+
+}
+
+module.exports = new DataBase();
